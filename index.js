8503ddc55367d2eeedaea6984b4169f882498974
Bliss <blissmaake140@gmail.com>
2022-11-28 21:49:04 +0300

    Done Assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solutions in this file
+const names = ['Guadalupe', 'Ollie', 'Aki'];
+
+function writeCards(names, event) {
+    const cards = [];
+
+    for (let i = 0; i < names.length; i++) {
+
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    }
+    console.log(cards)
+
+    return cards;
+}
+writeCards (names)
+
+
+function countDown (number) {
+    while (number >= 0) {
+        console.log (number);
+        number -= 1;
+    }
+}
+countDown(10)
